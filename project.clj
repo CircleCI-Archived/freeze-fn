225c4bb4b22ae9109b4b7be06df928e6b00d53fb
Allen Rohner <arohner@gmail.com>
2012-04-01 23:45:02 -0700

    Fix stupidism in the name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject circle.freeze-fn "0.1.0"
+(defproject circle/freeze-fn "0.1.0"
   :description "A library for serializing Clojure fn calls"
   :dependencies []
   :aot [circle.freeze-fn])
